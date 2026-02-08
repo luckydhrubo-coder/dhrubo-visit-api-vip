8d3f0a059330efea78c43fb7d7247d488a557c1c
DHRUBO <luckydhrubo@gmail.com>
2026-02-08 21:58:01 +0600

    Include developer details in player_info_response
    Added developer information to player info response.

--- app.py
+++ app.py
@@ -181,17 +181,20 @@ def send_visits():
     ))
 
     if player_info:
-        player_info_response = {
-            "fail": target_success - total_success,
-            "level": player_info.get("level", 0),
-            "likes": player_info.get("likes", 0),
-            "nickname": player_info.get("nickname", ""),
-            "region": player_info.get("region", ""),
-            "success": total_success,
-            "uid": player_info.get("uid", 0),
-            "tokens_used": len(tokens),
-            "token_rotation": "active"
-                }
+    player_info_response = {
+        "fail": target_success - total_success,
+        "level": player_info.get("level", 0),
+        "likes": player_info.get("likes", 0),
+        "nickname": player_info.get("nickname", ""),
+        "region": player_info.get("region", ""),
+        "success": total_success,
+        "uid": player_info.get("uid", 0),
+        "tokens_used": len(tokens),
+        "token_rotation": "active",
+        "developer": "DHRUBO",
+        "developer_telegram_user_name": "DHRUBO_X_TCP",
+        "about_channel": "https://t.me/who_is_dhrubo"
+    }
         return jsonify(player_info_response), 200
     else:
         return jsonify({"error": "Could not decode player information"}), 500
